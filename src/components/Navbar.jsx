2cab9855d138e9ed621fd9fef2d30e3d12f523f3
tomekkzielinski <tomasz.zielinski98tz@gmail.com>
2024-01-11 22:18:58 +0100

    zmiana w komponencie menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,38 @@
-import {Link, NavLink} from 'react-router-dom';
-import './Navbar.css'
-import React, { useState } from 'react';
-
-
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
 
-    const [menuOpen,setMenuOpen] =  useState(false); 
-
-    const menuHandler = () => {
-        setMenuOpen(!menuOpen);
-    };
+  const menuHandler = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <nav>
-       
-        <Link to='/' className="title">
-          <img src="../../coffee-logo-1.png" alt="Coffee-App Logo" />
-        </Link>
-  
+      <Link to="/" className="title">
+        <img src="../../coffee-logo-1.png" alt="Coffee-App Logo" />
+      </Link>
 
-
-        <div className="menu" onClick={menuHandler}>
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
+      <div className="menu" onClick={menuHandler}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
       <ul className={menuOpen ? "open" : ""}>
-        <li><NavLink to='menu'>Menu</NavLink></li>
-        <li><NavLink to='services'>Promocje</NavLink></li>
-        <li><NavLink to='contact'>Kontakt</NavLink></li>
-        <li><NavLink to='about'>O nas</NavLink></li>
+        <li>
+          <NavLink to="menu">Menu</NavLink>
+        </li>
+        <li>
+          <NavLink to="services">Promocje</NavLink>
+        </li>
+        <li>
+          <NavLink to="contact">Kontakt</NavLink>
+        </li>
+        <li>
+          <NavLink to="about">O nas</NavLink>
+        </li>
       </ul>
     </nav>
   );
