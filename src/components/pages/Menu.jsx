45cc29aa092ea2865d95003a84d6808012a7d6a5
tomekkzielinski <tomasz.zielinski98tz@gmail.com>
2024-03-06 21:54:31 +0100

    delete product handling

--- src/components/pages/Menu.jsx
+++ src/components/pages/Menu.jsx
@@ -1,31 +1,40 @@
-import React, { useState, useEffect } from "react";
-import Product from "./Product";
-import axios from "axios";
+import React, { useState, useEffect } from 'react';
+import Product from './Product';
+import axios from 'axios';
 
 const Menu = () => {
-  const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState([]);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        const response = await axios.get("http://localhost:5000/products");
-        setProducts(response.data);
-      } catch (error) {
-        console.error(error);
-      }
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get('http://localhost:5000/products');
+                setProducts(response.data);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const handleDelete = async (productId) => {
+        try {
+            await axios.delete(`http://localhost:5000/products/${productId}`);
+            setProducts(products.filter(product => product.id !== productId));
+        } catch (error) {
+            console.error('Error deleting product:', error);
+        }
     };
 
-    fetchProducts();
-  }, []);
-
-  return (
-    <div>
-      <p className="text-4xl font-bold ml-4 mt-6">Kawa:</p>
-      <div className="flex justify-center items-center">
-        <Product products={products} />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <p className="text-4xl font-bold ml-4 mt-6">Kawa:</p>
+            <div className="flex justify-center items-center">
+                <Product products={products} onDelete={handleDelete} />
+            </div>
+        </div>
+    );
 };
 
 export default Menu;
--- src/components/pages/Product.jsx
+++ src/components/pages/Product.jsx
@@ -1,31 +1,19 @@
 import React from 'react';
 import axios from 'axios';
 
-const Product = ({ products, onDelete }) => { 
-    const handleDelete = async (productId) => {
-        try {
-            console.log(products)
-            await axios.delete(`http://localhost:5000/products/${productId}`);
-            onDelete(productId); // Aktualizacja stanu po usunięciu produktu
-        } catch (error) {
-            console.error('Error deleting product:', error);
-        }
-        
-    };
-    
+const Product = ({ products, onDelete }) => {
     return (
         <div className="gap-4 m-5 flex flex-wrap">
-            {products.map((product, index) => (
-                <div key={index} className="card w-96 bg-base-100 shadow-xl">
-                    <figure><img src={product.image} alt="Coffee" /></figure>
+            {products.map(product => (
+                <div key={product.id} className="card w-96 bg-base-100 shadow-xl">
+                    <figure><img src={product.image} alt="Shoes" /></figure>
                     <div className="card-body">
                         <h2 className="card-title text-xl font-bold">{product.name}</h2>
                         <p className="text-lg">{product.description}</p>
                         <p className="font-bold text-lg">{product.price} zł</p>
                         <div className="card-actions justify-end">
-                            <button className="btn bg-main-color">Kup teraz!</button>
-                            
-                            <button className="btn bg-main-color" onClick={() => handleDelete(product.id)}>Usuń</button>
+                        <button className="btn bg-main-color" >Kup teraz!</button>
+                            <button className="btn bg-main-color" onClick={() => onDelete(product.id)}>Usuń</button>
                         </div>
                     </div>
                 </div>
