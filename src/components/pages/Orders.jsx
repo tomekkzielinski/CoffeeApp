ef8d106af8b413f9f953a2b3feb35eafe9f0c410
tomekkzielinski <tomasz.zielinski98tz@gmail.com>
2024-04-22 17:57:09 +0200

    reworks

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -20,67 +20,80 @@ const Orders = () => {
   }, []);
 
   // Funkcja do grupowania zamówień po session_id
-  const groupOrdersBySessionId = orders => {
+  const groupOrdersBySessionId = (orders) => {
     const groupedOrders = {};
-    orders.forEach(order => {
+    orders.forEach((order) => {
       const sessionId = order.session_id;
       if (!groupedOrders[sessionId]) {
         groupedOrders[sessionId] = {
           products: [],
-          totalPrice: 0
+          totalPrice: 0,
         };
       }
       groupedOrders[sessionId].products.push({
         name: order.product_name,
-        quantity: order.quantity
+        quantity: order.quantity,
       });
       groupedOrders[sessionId].totalPrice += order.total_price;
     });
     return groupedOrders;
   };
   const handleDelete = (session_id) => {
-    console.log("Session ID to delete:", session_id);  // Dodaj logowanie, aby sprawdzić przekazywaną wartość
+    console.log("Session ID to delete:", session_id); // Dodaj logowanie, aby sprawdzić przekazywaną wartość
 
     // Sprawdź, czy session_id jest poprawnie przekazywane i jest stringiem
-    if (typeof session_id === 'string') {
-        axios.delete(`http://localhost:5000/orders/${session_id}`)
-            .then(response => {
-                console.log('Response:', response.data);
-                const updatedOrders = {...groupedOrders};
-                delete updatedOrders[session_id];
-                setGroupedOrders(updatedOrders);
-            })
-            .catch(error => {
-                console.error('Error deleting orders:', error);
-            });
+    if (typeof session_id === "string") {
+      axios
+        .delete(`http://localhost:5000/orders/${session_id}`)
+        .then((response) => {
+          console.log("Response:", response.data);
+          const updatedOrders = { ...groupedOrders };
+          delete updatedOrders[session_id];
+          setGroupedOrders(updatedOrders);
+        })
+        .catch((error) => {
+          console.error("Error deleting orders:", error);
+        });
     } else {
-        console.error("Invalid session_id type:", typeof session_id);
+      console.error("Invalid session_id type:", typeof session_id);
     }
-};
-  
+  };
 
   return (
     <div className="m-10 flex flex-row flex-wrap">
-      {Object.entries(groupedOrders).map(([sessionId, { products, totalPrice }]) => (
-        <div key={sessionId} className=" m-5 card w-96 bg-main-color text-font-color">
-          <div className="card-body">
-            <h2 className="card-title text-lg justify-center mb-10 font-bold">Numer zamówienia: {sessionId}</h2>
-            {products.map((product, index) => (
-              <div key={index}>
-                <p>
-                  Product Name: {product.name} x {product.quantity}
-                </p>
+      {Object.entries(groupedOrders).map(
+        ([sessionId, { products, totalPrice }]) => (
+          <div
+            key={sessionId}
+            className=" m-5 card w-96 bg-main-color text-font-color"
+          >
+            <div className="card-body">
+              <h2 className="card-title text-lg justify-center mb-10 font-bold">
+                Numer zamówienia: {sessionId}
+              </h2>
+              {products.map((product, index) => (
+                <div key={index}>
+                  <p>
+                    Product Name: {product.name} x {product.quantity}
+                  </p>
+                </div>
+              ))}
+              <p className="text-2xl font-bold mt-5">
+                Total Price: ${totalPrice}
+              </p>
+              <div className="grid justify-items-end">
+                <button
+                  className="btn bg-buttons-color text-white"
+                  onClick={() => handleDelete(sessionId)}
+                  disabled={deleted}
+                >
+                  {deleted ? "Zrealizowano" : "Usuń zamówienia"}
+                </button>
               </div>
-            ))}
-            <p className="text-2xl font-bold mt-5">Total Price: ${totalPrice}</p>
-            <div className="grid justify-items-end">
-            <button className="btn bg-buttons-color text-white" onClick={() => handleDelete(sessionId)} disabled={deleted}>
-    {deleted ? 'Zrealizowano' : 'Usuń zamówienia'}
-</button>
             </div>
           </div>
-        </div>
-      ))}
+        )
+      )}
     </div>
   );
 };
